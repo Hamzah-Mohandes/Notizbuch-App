b446ba2a3218e963ba8fc1d025c01808604f224f
Hamzah-Mohandes <rootmoodrm@gmail.com>
2023-05-15 09:53:59 +0200

    Finish  9:54 --- PPBSN

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,45 @@
-fun main(args: Array<String>) {
-  val note = Note("start", "stop")
-   note.anzeigen()
+fun main() {
+    val notizbuch = Notebook()
+    var beenden = false
+
+    while (!beenden){
+        println("Was möchtest du tun?")
+        println("1. Notizen anzeigen")
+        println("2. Notiz hinzufügen")
+        println("3. Notiz löschen")
+        println("4. Beenden")
+        val auswahl1 = readLine()
+
+        when (auswahl1){
+
+            "1" ->{
+                println("Notizen im Notizbuch: ")
+                notizbuch.anzeigen()
+            }
+            "2" ->{
+                println("Geben Sie den Titel der Notiz ein: ")
+                val titel = readLine()
+                println("Geben Sie den Inhalt der Notiz ein: ")
+                val inhalt = readLine()
+                val neueNotiz = Note(titel ?: "", inhalt ?: "")
+                notizbuch.hinzufügen(neueNotiz)
+                println("Notiz hinzugefügt")
+            }
+            "3" -> {
+                println("Geben Sie den Index der zu löschenden Notiz ein: ")
+                val index = readLine()?.toIntOrNull()
+                if (index != null) {
+                    notizbuch.löschen(index)
+                    println("Notiz gelöscht")
+                } else {
+                    println("Ungültiger Index.")
+                }
+            }
+            "4" -> {
+                beenden = true
+                println("Anwendung beendet.")
+            }
+            else -> println("Ungültige Auswahl.")
+        }
+    }
 }
--- src/main/kotlin/Notebook.kt
+++ src/main/kotlin/Notebook.kt
@@ -13,6 +13,25 @@ löschen(index): Löscht eine Note aus dem Notebook basierend auf dem Index.
      */
     val notizen : MutableList<Note> = mutableListOf()
     fun anzeigen (){
-
+if (notizen.isEmpty()) {
+    println("Notizen empty")
+} else {
+    for ((index, note) in notizen.withIndex()) {
+        println("Note: $index ")
+        note.anzeigen()
     }
 }
+    }
+    fun hinzufügen(notiz: Note){
+       notizen.add(notiz)
+        println("Notiz Hinzufügt")
+    }
+
+    fun löschen(index: Int){
+        if (index in 0 until notizen.size){
+            notizen.removeAt(index)
+            println("Notiz hat gelöschet")
+    } else {
+        println("Ungülting Index")
+    }
+}}
